f6ac189ba0ed0bc7878e7048f62534bc3a72c963
NataliiaShevchenko620 <shevchenkon620@gmail.com>
2024-05-20 20:09:05 -0400

    Added source data

--- Team/API/get_employment_trend/sql/get_employment_trend_ca.sql
+++
@@ -1,8 +0,0 @@
-SELECT m.year,
-        SUM(bls_annual_employment) AS metric
-        FROM county_industry_metric m
-        INNER JOIN
-        county c ON c.county_fips = m.county_fips
-        WHERE c.state_code = 'CA'
-        AND industry_code = 1011
-GROUP BY m.year
--- Team/API/get_employment_trend/sql/get_employment_trend_us.sql
+++
@@ -1,5 +0,0 @@
-SELECT year,
-        SUM(bls_annual_employment) AS metric
-        FROM county_industry_metric
-        WHERE industry_code = 1011
-GROUP BY year
